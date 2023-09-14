1b5e78000b3bc4fc9fdbd9606bf4520a17bf80c5
Angel <125516909+adop05@users.noreply.github.com>
2023-09-14 16:59:38 -0700

    Create grid function

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-const grid = document.getElementById('.grid')
+createBoard(16)
+function createBoard(size){
+    const grid = document.querySelector('.grid')
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
+
+    for (let i = 0; i < size * size; i++){
+        const gridElement = document.createElement('div')
+        gridElement.classList.add('grid-element')
+        grid.appendChild(gridElement)
+    }
+
+}
